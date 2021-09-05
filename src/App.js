7d39085ba2db5a49c58ff2fc34a6ed159bc0305d
NiDaSanti <santiago.nicholas.a@gmail.com>
2021-09-04 23:14:07 -0700

    added the routes for pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from "./components/Home/home.js";
+import About from "./components/About/about.js";
+import Dashboard from "./components/Dashboard/dashboard.js";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+          <hr />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
