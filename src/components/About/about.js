49bf7984645d6eb552ce8a2a5232b486bdf28a0b
NiDaSanti <santiago.nicholas.a@gmail.com>
2021-09-05 15:13:12 -0700

    adding material-ui in about me and added content

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,7 +1,60 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import {Container} from "@material-ui/core";
+import "./styles.css";
 const About = () => {
+  const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
   return(
-    <h1>Hello About</h1>
+    <div className="user-container">
+      <Card className={makeStyles.root}>
+        <CardActionArea>
+          <CardMedia
+            className={makeStyles.media}
+            image="/images/IMG_8142.jpg"
+            title="Nicholas Santiago"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Nicholas Santiago
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Nick is a ten year veteran and a Sofware Developer.
+              </Typography>
+            </CardContent>
+        </CardActionArea>
+      </Card>
+      <Card className={makeStyles.root}>
+        <CardActionArea>
+          <CardMedia
+            className={makeStyles.media}
+            image="/images/IMG_8142.jpg"
+            title="Nicholas Santiago"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Kofa Freeman
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Kofa is a ten year veteran and a Business Professional.
+              </Typography>
+            </CardContent>
+        </CardActionArea>
+      </Card>
+    </div>
   );
 }
 export default About;
