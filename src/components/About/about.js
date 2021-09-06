9897282b9980b4d80dad5dbc0f1a2b671fe14c3a
NiDaSanti <santiago.nicholas.a@gmail.com>
2021-09-05 19:39:58 -0700

    crating container for bio

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -3,20 +3,34 @@ import "./styles.css";
 const About = () => {
   return(
     <div className="container">
-      <div className="card" >
-        <img className="card-img-top" src="IMG_8147.jpg" alt="Card image cap"/>
-        <div className="card-body">
-          <h2>Nicholas Santiago</h2>
-          <p className="card-text">Nick is a ten year veteran and currently a Software Developer.</p>
+      <div className="bio-container">
+        <div className="card" >
+          <img className="card-img-top" src="IMG_8147.jpg" alt="Card image cap"/>
+            <div className="card-body">
+              <h2 className="text-danger">Nick Santiago</h2>
+              <p className="card-text">Nick is a ten year veteran and currently a Software Developer.</p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              This is some text within a card body.
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="card" >
-        <img className="card-img-top" src="IMG_8151.jpg" alt="Card image cap"/>
-        <div className="card-body">
-          <h2>Kofa Freeman</h2>
-          <p className="card-text">Kofa is a ten year veteran and a Business Professional.</p>
+      <div className="bio-container">
+        <div className="card" >
+          <img className="card-img-top" src="IMG_8151.jpg" alt="Card image cap"/>
+            <div className="card-body">
+              <h2 class="text-danger">Kofa Freeman</h2>
+              <p className="card-text">Kofa is a ten year veteran and a Business Professional.</p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              This is some text within a card body.
+            </div>
+          </div>
         </div>
-      </div>
     </div>
   );
 }
