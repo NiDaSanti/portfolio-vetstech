26c997b13b7d1070bcff99d38029c056d47ebacc
NiDaSanti <santiago.nicholas.a@gmail.com>
2021-09-06 13:04:22 -0700

    almost done with about me. I need real text, not dummy

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,33 +1,42 @@
 import React from "react";
 import "./styles.css";
 const About = () => {
+  const increaseWidthOfCard = {
+    width: "50rem",
+  };
   return(
     <div className="container">
       <div className="bio-container">
-        <div className="card" >
-          <img className="card-img-top" src="IMG_8147.jpg" alt="Card image cap"/>
+        <div className="card">
+          <img className="card-img-top" src="IMG_8147.jpg" alt="Nick"/>
             <div className="card-body">
               <h2 className="text-danger">Nick Santiago</h2>
               <p className="card-text">Nick is a ten year veteran and currently a Software Developer.</p>
             </div>
           </div>
-          <div class="card">
-            <div class="card-body">
-              This is some text within a card body.
+          <div className="card" style={increaseWidthOfCard}>
+            <div className="card-body">
+            Non etiam tempor id arcu magna ante eget. Nec per posuere cubilia cras porttitor condimentum
+  orci suscipit. Leo maecenas in tristique, himenaeos elementum placerat. Taciti rutrum nostra,
+  eget cursus velit ultricies. Quam molestie tellus himenaeos cubilia congue vivamus ultricies.
+  Interdum praesent ut penatibus fames eros ad consectetur sed
             </div>
           </div>
         </div>
       <div className="bio-container">
         <div className="card" >
-          <img className="card-img-top" src="IMG_8151.jpg" alt="Card image cap"/>
+          <img className="card-img-top" src="IMG_8151.jpg" alt="Kofa"/>
             <div className="card-body">
-              <h2 class="text-danger">Kofa Freeman</h2>
+              <h2 className="text-danger">Kofa Freeman</h2>
               <p className="card-text">Kofa is a ten year veteran and a Business Professional.</p>
             </div>
           </div>
-          <div class="card">
-            <div class="card-body">
-              This is some text within a card body.
+          <div className="card" style={increaseWidthOfCard}>
+            <div className="card-body">
+            Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius;
+  dignissim elementum. Mollis tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.
+  Hendrerit parturient habitant pharetra rutrum gravida porttitor eros feugiat. Mollis elit
+  sodales taciti duis praesent id. Consequat urna vitae morbi nunc congue.
             </div>
           </div>
         </div>
