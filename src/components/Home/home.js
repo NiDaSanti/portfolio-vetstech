0d5376b22a5a93856605687b649ac5eb0e825c19
NiDaSanti <santiago.nicholas.a@gmail.com>
2021-10-03 16:27:14 -0700

    basic RWD for now

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,44 +1,17 @@
 import React, {useState} from "react";
 import {Carousel} from "react-carousel-minimal";
+import "./home.css";
 const Home = () => {
-  const cardStyle = {
-    width: "auto",
-    marginLeft: "auto",
-    marginRight: "auto",
-  };
-  const makeStrong = {
-    fontWeight: "bold",
-  };
-  const contain = {
-    position: "absolute",
-    top: "35%",
-    left: "40%",
-    tranformed: "translate(-50%, -50%)",
-    color: "#fff",
-    width: "50rem",
-  };
-  const stylePic = {
-    width: "auto",
-    height: "800px",
-  };
-  const text = {
-    fontSize: "2.5rem",
-    textShadow: "0 1px 0 rgba(255, 255, 255, 0.4)",
-  }
-  // const styleStatement = {
-  //   width: "30rem",
-  //   height: "40rem",
-  // };
   const missionStatementText = "To build, educate, help and " +
                                "empowered veterans through " +
                                "innovation and technology.";
     return(
     <>
-      <div className="card" style={cardStyle}>
-        <img src="soldier-996536_1920.jpg" style={stylePic} className="img-fluid" alt="Soldier Soluting" />
-        <article style={contain}>
-          <h1 style={text}>Mission</h1>
-          <h2 style={text}><q>{missionStatementText}</q></h2>
+      <div className="card-style">
+        <img src="soldier-996536_1920.jpg" className="img-fluid custom-soldier" alt="Soldier Soluting" />
+        <article className="contain">
+          <h1 className="text">Mission</h1>
+          <h2 className="text"><q>{missionStatementText}</q></h2>
         </article>
       </div>
     </>
